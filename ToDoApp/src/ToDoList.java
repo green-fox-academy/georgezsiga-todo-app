e3e16eebe935fe3d6f144e2c7f00a48d27a5ddef
georgezsiga <georgezsiga@gmail.com>
2017-04-06 12:56:07 +0200

    Add constructor

--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,6 +1,14 @@
+
+import java.util.ArrayList;
+
 /**
  * Created by georgezsiga on 4/6/17.
  */
 public class ToDoList {
+  ArrayList<String> taskList;
+
+  public ToDoList() {
+    this.taskList = new ArrayList();
+  }
 
 }
