a8e8bc3aa77586a67d7f7328ce4affeb96d9b493
georgezsiga <georgezsiga@gmail.com>
2017-04-06 13:40:01 +0200

    Add the addtask function

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by georgezsiga on 4/6/17.
  */
@@ -5,11 +10,21 @@ public class App {
 
   public static void main(String[] args) {
     ArgumentHandler handler = new ArgumentHandler(args);
+    ToDoList list = new ToDoList();
 
     if (handler.contains("l")) {
-      handler.listTheTasks();
+      list.listTheTasks();
     } else if (handler.contains("a")){
-      handler.addNewTask();
+      String taskName = args[1].toString();
+      list.taskList.add(taskName);
+      try{
+        Path filePath = Paths.get("files/data.txt");
+        Files.write(filePath, list.taskList);
+      } catch (IOException e) {
+        e.printStackTrace();
+        System.out.println("Uh-oh, could not write the file!");
+      }
+      System.out.println("Your task: '" + taskName + "' has been added to your task list");
     } else {
       handler.noArgument();
     }
