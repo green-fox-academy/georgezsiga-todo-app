f1861e77eed8d0642b160328cf9b150de42d6b7b
georgezsiga <georgezsiga@gmail.com>
2017-04-06 12:01:56 +0200

    Moves the noArgument method to ArgumentHandler

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,29 +1,7 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Scanner;
-
 /**
  * Created by georgezsiga on 4/6/17.
  */
 public class App {
-
-  public void noArgument() {
-    System.out.println();
-    System.out.println("Java ToDo application");
-    System.out.println("=====================");
-    System.out.println();
-    System.out.println("Command line arguments:");
-    System.out.println(" -l  Lists all the tasks");
-    System.out.println(" -a  Adds a new task");
-    System.out.println(" -r  Removes a task");
-    System.out.println(" -c  Completes a task");
-    System.out.println();
-    System.out.println("So, what do you want to do?");
-    System.out.println();
-  }
 
   public static void main(String[] args) {
     ArgumentHandler handler = new ArgumentHandler(args);
@@ -32,7 +10,7 @@ public class App {
     if (handler.contains("l")) {
       handler.listTheTasks();
     } else {
-      app.noArgument();
+      handler.noArgument();
     }
   }
 }
