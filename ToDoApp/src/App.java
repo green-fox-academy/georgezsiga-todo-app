f09e0f620ec3a27ffcac88b73b8d4285a32ec8e8
georgezsiga <georgezsiga@gmail.com>
2017-04-06 17:41:05 +0200

    Add the check problem messages.

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * Created by georgezsiga on 4/6/17.
@@ -6,6 +7,8 @@ public class App {
   String[] args;
   String taskName;
   int taskNumber;
+  String remove = "remove";
+  String check = "check";
 
   public App(String[] args) {
     this.args = args;
@@ -33,13 +36,15 @@ public class App {
   }
 
   public int taskNumberMethod() {
+    ToDoList list = new ToDoList();
+    ArrayList<String> list2 = list.readFromFile();
     try {
       if (args.length == 1) {
         System.out.println("Unable to remove, no index provided");
         System.exit(0);
       } else if (Integer.parseInt(args[1]) < -1) {
 
-      } else if (args.length < Integer.parseInt(args[1])) {
+      } else if (list2.size() < Integer.parseInt(args[1])) {
         System.out.println("Unable to remove: index is out of bound");
         System.exit(0);
       } else {
@@ -47,6 +52,28 @@ public class App {
       }
     } catch (NumberFormatException e) {
       System.out.println("Unable to remove: index is not a number");
+      System.exit(0);
+    }
+    return taskNumber;
+  }
+
+  public int taskNumberMethodCheck() {
+    ToDoList list = new ToDoList();
+    ArrayList<String> list2 = list.readFromFile();
+    try {
+      if (args.length == 1) {
+        System.out.println("Unable to check, no index provided");
+        System.exit(0);
+      } else if (Integer.parseInt(args[1]) < -1) {
+
+      } else if (list2.size() < Integer.parseInt(args[1])) {
+        System.out.println("Unable to check: index is out of bound");
+        System.exit(0);
+      } else {
+        taskNumber = Integer.parseInt(args[1]);
+      }
+    } catch (NumberFormatException e) {
+      System.out.println("Unable to check: index is not a number");
       System.exit(0);
     }
     return taskNumber;
@@ -70,7 +97,6 @@ public class App {
   public static void main(String[] args) {
     App handler = new App(args);
     ToDoList list = new ToDoList();
-
     if (handler.contains("l")) {
       list.listTheTasks();
     } else if (handler.contains("a")){
@@ -79,6 +105,9 @@ public class App {
     } else if (handler.contains("r")) {
       int number = handler.taskNumberMethod();
       list.removeTask(number);
+    } else if (handler.contains("c")) {
+      int number = handler.taskNumberMethodCheck();
+      list.replaceTask(number);
     } else if (handler.args.length < 1){
       handler.noArgument();
     } else {
