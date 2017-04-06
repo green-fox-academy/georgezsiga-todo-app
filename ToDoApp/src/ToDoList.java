45c51e2a3e83e7c3a7d5655d2aa22d579c7d3cc1
georgezsiga <georgezsiga@gmail.com>
2017-04-06 13:38:54 +0200

    Add list the tasks method

--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -1,5 +1,10 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by georgezsiga on 4/6/17.
@@ -8,7 +13,23 @@ public class ToDoList {
   ArrayList<String> taskList;
 
   public ToDoList() {
-    this.taskList = new ArrayList();
+    this.taskList = new ArrayList<>();
+  }
+
+  public void listTheTasks() {
+    try {
+      Path filePath = Paths.get("files/data.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      if (lines.size() == 0) {
+        System.out.println("Good job, no ToDo`s for today! Go and have some fun!");
+      } else {
+        for (int i = 0; i < lines.size(); i++) {
+          System.out.println(" " + i + 1 + " " + lines.get(i));
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
 }
