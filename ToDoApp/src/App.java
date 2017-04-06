a48431de0cac6906dd35095d9233eec9e1611074
georgezsiga <georgezsiga@gmail.com>
2017-04-06 10:08:34 +0200

    Add the usage information

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -4,6 +4,15 @@
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(args);
+    System.out.println("Java Todo application");
+    System.out.println("=====================");
+    System.out.println();
+    System.out.println("Command line arguments:");
+    System.out.println(" -l  Lists all the tasks");
+    System.out.println(" -a  Adds a new task");
+    System.out.println(" -r  Removes a task");
+    System.out.println(" -c  Completes a task");
+    System.out.println();
+    System.out.println("So, what do you want to do?");
   }
 }
