2208e88d7bf93e21f694a292152e28da9665b8f1
georgezsiga <georgezsiga@gmail.com>
2017-04-06 22:06:55 +0200

    Move methods from ToDo List file

--- ToDoApp/src/ToDo.java
+++ ToDoApp/src/ToDo.java
@@ -2,5 +2,36 @@
  * Created by georgezsiga on 4/6/17.
  */
 public class ToDo {
+  ToDoList list = new ToDoList();
+
+  public void addNewTask(String string) {
+    list.readFromFile();
+    String string2 = " " + string;
+    list.taskList.add(string2);
+    System.out.println("Your task: '" + string + "' has been added to your task list");
+    list.printToFile();
+  }
+
+  public void removeTask(int number) {
+    list.readFromFile();
+    int indexNumber = number -1;
+    list.taskList.remove(indexNumber);
+    System.out.println(
+        "The task at this position: '" + number + "' has been removed from your task list");
+    list.printToFile();
+  }
+
+  public void replaceTask(int number) {
+    list.readFromFile();
+    int indexNumber = number -1;
+    String string = list.taskList.get(indexNumber);
+    String check = string.substring(0,1);
+    String string2 = string.substring(1);
+    string = "x" + string2;
+    list.taskList.set(indexNumber, string);
+    System.out.println(
+        "The task at this position: '" + number + "' has been checked as done in your task list");
+    list.printToFile();
+  }
 
 }
