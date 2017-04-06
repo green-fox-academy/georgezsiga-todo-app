71cc77768674809f815a00355a9e9dc7872e16d3
georgezsiga <georgezsiga@gmail.com>
2017-04-06 15:42:47 +0200

    Add read from file method

--- ToDoApp/src/ToDoList.java
+++ ToDoApp/src/ToDoList.java
@@ -10,6 +10,7 @@ import java.util.List;
  * Created by georgezsiga on 4/6/17.
  */
 public class ToDoList {
+
   private ArrayList<String> taskList;
 
   public ToDoList() {
@@ -20,29 +21,46 @@ public class ToDoList {
     return taskList;
   }
 
-  public ArrayList<String> addNewTask(String string) {
-    taskList.add(taskList.size(), string);
+  public void addNewTask(String string) {
+    readFromFile();
+    taskList.add(string);
     System.out.println("Your task: '" + string + "' has been added to your task list");
     System.out.println(taskList);
+    printToFile();
+  }
+
+  public ArrayList<String> readFromFile() {
+    try {
+      Path filePath = Paths.get("files/data.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      for (int i = 0; i < lines.size(); i++) {
+        taskList.add(i, lines.get(i));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return taskList;
   }
 
-  public ArrayList<String> removeTask(int number) {
-    taskList.remove(number);
-    System.out.println("The task at this position: '" + number + "' has been added to your task list");
-    System.out.println(taskList);
-    return taskList;
+  public int removeTask(int number) {
+    readFromFile();
+    int indexNumber = number -1;
+    taskList.remove(indexNumber);
+    System.out.println(
+        "The task at this position: '" + number + "' has been removed from your task list");
+    printToFile();
+    return indexNumber;
   }
 
   public void printToFile() {
-    try{
+    try {
       Path filePath = Paths.get("files/data.txt");
       Files.write(filePath, getTaskList());
     } catch (IOException e) {
       e.printStackTrace();
       System.out.println("Uh-oh, could not write the file!");
     }
-//    System.out.println("Your task: '" + string + "' has been added to your task list");
+    System.out.println("Your tasklist has been refreshed");
   }
 
   public void listTheTasks() {
@@ -53,7 +71,7 @@ public class ToDoList {
         System.out.println("Good job, no ToDo`s for today! Go and have some fun!");
       } else {
         for (int i = 0; i < lines.size(); i++) {
-          System.out.println(" " + i + " " + lines.get(i));
+            System.out.println(" " + (i+1) + " " + lines.get(i));
         }
       }
     } catch (IOException e) {
