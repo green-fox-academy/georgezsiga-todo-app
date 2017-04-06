46462dbf55264def582692eb1c84cfdb86259215
georgezsiga <georgezsiga@gmail.com>
2017-04-06 10:50:07 +0200

    Add list the tasks method

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,11 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by georgezsiga on 4/6/17.
  */
 public class App {
 
+  public void listTheTasks() {
+    try {
+      Path filePath = Paths.get("data.txt");
+      List<String> lines = Files.readAllLines(filePath);
+      System.out.println(lines);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
   public static void main(String[] args) {
+    App app = new App();
     System.out.println();
-    System.out.println("Java Todo application");
+    System.out.println("Java ToDo application");
     System.out.println("=====================");
     System.out.println();
     System.out.println("Command line arguments:");
@@ -16,5 +34,10 @@ public class App {
     System.out.println();
     System.out.println("So, what do you want to do?");
     System.out.println();
+    Scanner scanner = new Scanner(System.in);
+    String userInput = scanner.nextLine();
+    if (userInput == "-l") {
+      app.listTheTasks();
+    }
   }
 }
